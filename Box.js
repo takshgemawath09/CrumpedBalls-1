3a66061deea1fe025834714e2ef3b80e2e44eabe
takshgemawath09 <72132215+takshgemawath09@users.noreply.github.com>
2020-12-08 10:10:14 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,6 +1,7 @@
 class Box {
     constructor(x, y, width, height) {
       var options = { 
+          isStatic:true,
           'restitution':0.8,
           'friction':20.0,
           'density':1.0
